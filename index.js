c1d2cea06cd370476a32e7b25647ffc4e9832fbc
Natalia Sofia <nataliasofiamatos@gmail.com>
2022-02-10 16:36:05 -0500

    inicial comments

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+const showComment = async (event) => {
+  let comment = await getcommentsFromDatabase(event.target.id);
+  let parent = event.target.parentElement
+
+    const renderComment = (commentObj) => {
+      const newComment = `<div class='shown'>${commentObj.body}</div>`
+        parent.innerHTML = parent.innerHTML + newComment
+    }
+    comment.forEach(renderComment)
+}
+
 
 const app = async () => {
   let post = await getpostFromDatabase();
 
   const renderPost = (postObj) => {
-    const newPost = `<div><h3>${postObj.title}</h3><div>${postObj.body}</div></div>`
+    const newPost = `<div><h3>${postObj.title}</h3><div>${postObj.body}</div><button id=${postObj.id} onclick='showComment(event)' >Show Comments</button></div>`
     let data = document.querySelector('.post-container')
     data.innerHTML = data.innerHTML + newPost
   }
